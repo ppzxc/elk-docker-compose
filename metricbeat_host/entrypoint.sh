02cf855f41c0cec22d817afa55f5f3e3c5db3c85
JungHa-Cho <cjh8497@naver.com>
2020-01-10 18:17:20 +0900

    fix

--- metricbeat_host/entrypoint.sh
+++ metricbeat_host/entrypoint.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
-while ! curl -s -X GET http://172.18.0.2:9200/_cluster/health\?wait_for_status\=yellow\&timeout\=60s | grep -q '"status":"yellow"'
+while ! curl -s -X GET http://172.18.0.2:9200/_cluster/health\?wait_for_status\=green\&timeout\=60s | grep -q '"status":"green"'
 do
-    echo "==> Waiting for cluster YELLOW status" && sleep 1
+    echo "==> Waiting for cluster green status" && sleep 1
+done
+
+while ! curl -s -X GET http://172.18.0.3:9200/_cluster/health\?wait_for_status\=green\&timeout\=60s | grep -q '"status":"green"'
+do
+    echo "==> Waiting for cluster green status" && sleep 1
+done
+
+while ! curl -s -X GET http://172.18.0.4:9200/_cluster/health\?wait_for_status\=green\&timeout\=60s | grep -q '"status":"green"'
+do
+    echo "==> Waiting for cluster green status" && sleep 1
 done
 
 echo ""
-echo "Cluster is YELLOW. Fine ! (But you could maybe try to have it GREEN ;))"
+echo "Cluster is green. Fine ! (But you could maybe try to have it GREEN ;))"
 echo ""
 
-while ! curl -s -I http://172.18.0.5:5601/api/status | grep "200 OK" > /dev/null
+while ! curl -s -I http://172.18.0.9:5601/api/status | grep "200 OK" > /dev/null
 do
     echo "==> Waiting for kibana status" && sleep 1
 done
-
-sleep 5s
 
 echo ""
 echo "kibana status up"
